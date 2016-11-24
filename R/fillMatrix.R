ca58f4a98f3e220864a3567e0b22f98641bc5587
Marcos <marcos.dee.oliveira@gmail.com>
2016-11-24 00:10:27 -0200

    fill matrix, but not with the right values. It's just symbolic

--- R/fillMatrix.R
+++ R/fillMatrix.R
@@ -3,8 +3,36 @@
 # col: number of matrix collums
 # h: not used
 
-fillMatrix<- function(r,row, col,h){
-  auxMat<-matrix(f(r),row,col) ;  # fills each matrix element with the return of function f(r)
-  print(auxMat);
-  # print(paste("h:",h));
+fillMatrix<- function(inputFunction,leftExtremePoint,rightExtremePoint,step){
+  #generate a mesh with points
+  meshPoints<-seq(leftExtremePoint,rightExtremePoint,step)
+
+  # generate an auxiliar matrix filled with "0"
+  # auxMat<-matrix(0,row,col)
+  auxMat<-matrix(0,4,4)
+
+  matrixLines= length(auxMat[,1])    # length of first column in M
+  matrixColumns = length(auxMat[1,]) # length of first line in M
+
+  # fills each matrix element
+  for(lineEl in 1:matrixLines){
+    for(colEl in 1:matrixColumns){
+      # iteration debug:
+      # auxMat[lineEl,colEl] = paste(lineEl,colEl)
+
+      # with the return of function f(r)
+      if(lineEl == colEl){
+        auxMat[lineEl,colEl]="f(r)"
+      }
+      else if(abs(lineEl - colEl)==1){
+        auxMat[lineEl,colEl]="-1"
+      }
+      else{
+        # unnecessary. auxMat begins filled with '0'
+        auxMat[lineEl,colEl]="0"
+      }
+    }
+
+  }
+  print(auxMat)
 }
